78b33eee21487e42c1f852f51a916b912ab6550b
Vechro <stenmr@gmail.com>
2018-06-20 23:03:33 +0300

    spaces to tabs

--- server.lua
+++ server.lua
@@ -2,22 +2,22 @@ local vehicles = {}
 
 RegisterNetEvent("onDeliveryCreated")
 AddEventHandler(
-    "onDeliveryCreated",
-    function(net, props)
-        print("Server:Creating delivery", net)
+	"onDeliveryCreated",
+	function(net, props)
+		print("Server:Creating delivery", net)
 
-        local source = source
-        local net = props.vehicle or 0
+		local source = source
+		local net = props.vehicle or 0
 
-        vehicles[net] = props or true
+		vehicles[net] = props or true
 
-        TriggerClientEvent("onDeliveryCreated", -1, net, vehicles)
-    end
+		TriggerClientEvent("onDeliveryCreated", -1, net, vehicles)
+	end
 )
 
 AddEventHandler(
-    "playerConnecting",
-    function()
-        TriggerClientEvent("onDeliveryCreated", source, 0, vehicles)
-    end
+	"playerConnecting",
+	function()
+		TriggerClientEvent("onDeliveryCreated", source, 0, vehicles)
+	end
 )
